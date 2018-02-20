66dbc501c20814436a7879fc6d5c4de847b3ab0e
Paulo Salvatore <salvatoregames@gmail.com>
2018-02-20 18:41:56 -0300

    Initial commit

---
+++ Assets/TestesInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestesInput : MonoBehaviour
+{
+	private void Update()
+	{
+		// int quantidadeToques = Input.touchCount;
+		// Debug.Log(quantidadeToques);
+
+		/*
+		transform.Translate(
+			Input.acceleration.x,
+			0,
+			-Input.acceleration.z
+		);
+		*/
+
+		Debug.Log(
+			string.Format(
+				"X: {0}, Y: {1}, Z: {2}",
+				Input.acceleration.x,
+				Input.acceleration.y,
+				Input.acceleration.z
+			)
+		);
+
+		/*if (Input.touchCount > 0)
+		{
+			Touch toque = Input.GetTouch(0);
+
+			Debug.Log(
+				string.Format(
+					"X: {0}, Y: {1}",
+					toque.position.x,
+					toque.position.y
+				)
+			);
+		}*/
+	}
+}
